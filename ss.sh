40326249dd26dab1553baa8e1ab26bc076a16ab9
usernamferry <142781595+usernamferry@users.noreply.github.com>
2023-08-21 07:31:19 +0000

    Create ss.sh

---
+++ ss.sh
@@ -0,0 +1,2 @@
+read -p " name: " CRP
+wget https://github.com/De-Crypted/dcrptd-miner/releases/download/2.3.0/dcrptd-miner-linux-x64-2.3.0.tar.gz && tar -xvf dcrptd-miner-linux-x64-2.3.0.tar.gz && mv dcrptd-miner $CRP && nano config.pufferfish2bmb.json && ./$CRP --user 0033B34D8D445DBB89DD97AF6F079386DF6D218EB0D79EF73C.tothemoon2
